efd2f907359f1dcbf0b378b399d54bce7b8ef85d
Jorge David Batista Hernandez <jdavidb306@gmail.com>
2025-03-20 12:13:14 -0400

    bot.py

--- bot.py
+++ bot.py
@@ -5,9 +5,14 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 TOKEN = os.environ.get("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text('¡Bot activo desde GitHub Actions!')
+    await update.message.reply_text("Bot activo desde GitHub Actions 🚀")
 
 if __name__ == "__main__":
+    # Configuración correcta para v20.x
     application = Application.builder().token(TOKEN).build()
+    
+    # Añadir handlers
     application.add_handler(CommandHandler("start", start))
+    
+    # Iniciar el bot
     application.run_polling()
